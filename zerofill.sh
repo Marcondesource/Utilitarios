40d621de966cba03d1ba7e3ed5f554ff1c308e91
Marcondesource <codeandframes@gmail.com>
2025-12-29 03:57:38 -0300

    Kkkkk

--- zerofill.sh
+++ zerofill.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
-read -p "Qual a partição?" particao
+if [ "$EUID" -eq 0 ]; then
+echo "Iniciando Script"
+else
+echo "Favor Iniciar com Sudo"
+exit 1
+fi
+   
+echo "Abrindo Lsblk"
+lsblk
+
+read -p "Qual a partição? Exemplo:/dev/sdx" particao
 echo "Primeira passada 1/3"
-sudo dd if=/dev/zero of=/dev/$particao bs=4M status=progress
+sudo dd if=/dev/zero of=$particao bs=4M status=progress
 echo "Segunda passada 2/3"
-sudo dd if=/dev/urandom of=/dev/$particao bs=4M status=progress
+sudo dd if=/dev/urandom of=$particao bs=4M status=progress
 echo "Terceira Passada 3/3"
-sudo dd if=/dev/zero of=/dev/$particao bs=4M status=progress
+sudo dd if=/dev/zero of=$particao bs=4M status=progress
 
 echo "Reforçando zerofill"
-sudo shred -v -n 3 -z /dev/$particao
+sudo shred -v -n 3 -z $particao
 
 read -p "Deseja formatar o armazenamento? s/n" format
 case $format in
@@ -16,9 +26,9 @@ s)echo "FAT32"
   echo "EXT4"
 read -p "Qual tipo de formato voce deseja?" fodas
 case $fodas in
-FAT32) mkfs.fat -F32 /dev/$particao
+FAT32) mkfs.fat -F32 $particao
 ;;
-EXT4) mkfs.ext4 /dev/$particao
+EXT4) mkfs.ext4 $particao
 esac
 ;;
 n) saindo...
